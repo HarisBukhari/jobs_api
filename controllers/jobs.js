2e43e15723027278e68720dafc9696cf06062d7d
Syed Haris Bukhari <syedharishussain86@gmail.com>
2022-07-23 11:05:58 -0700

    Update jobs.js

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -8,13 +8,24 @@ const createJob = async (req, res) => {
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({ job })
 }
+
 const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
     res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
+
 const getJob = async (req, res) => {
-    res.send('get job')
+    const { user: { userId }, params: { id: jobId } } = req
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No Job With Id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
+
 const updateJobs = async (req, res) => {
     res.send('Update jobs')
 }
