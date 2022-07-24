d678a8d4fccd747c088cd8fdc98124534113666a
Syed Haris Bukhari <syedharishussain86@gmail.com>
2022-07-24 07:01:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(express.json())
 // extra packages
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticationUser, jobRouter)
